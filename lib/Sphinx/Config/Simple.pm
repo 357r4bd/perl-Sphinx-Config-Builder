ea936992076923f9a44d1153a2780958b9307d03
B. Estrade <estrabd@gmail.com>
2013-10-28 22:39:06 -0500

    working on POD, unit tests

--- lib/Sphinx/Config/Simple.pm
+++ lib/Sphinx/Config/Simple.pm
@@ -209,44 +209,19 @@ Sphinx::Config::Simple - Perl extension creating dynamic Sphinx configuration fi
 =head1 SYNOPSIS
 
   use Sphinx::Config::Simple;
-  
 
 =head1 DESCRIPTION
 
-Stub documentation for Sphinx::Config::Simple, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+This module allows one to systematically construct a Sphinx configuration file that
+contains so many entries that it is best created dynamically.
 
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
-
-=head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+This module doesn't read in Sphinx configuration files, it simply allows one
+to construct and output a configuration file programmtically.
 
 =head1 AUTHOR
 
-User &, E<lt>patchbaz@nonetE<gt>
+B. Estrade, E<lt>estrabd@gmail.com<gt>
 
-=head1 CoPYRIGHT AND LICENSE
+=head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2013 by User &
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.14.4 or,
-at your option, any later version of Perl 5 you may have available.
-
-
-=cut
+Same terms as Perl itself.
